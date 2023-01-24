237a45bc30f7ce1f5610c17d181860aa14d46f97
GhadeerZahwe <zahweghadeer@gmail.com>
2023-01-24 13:33:16 +0200

    Index

--- Models/Transaction.cs
+++ Models/Transaction.cs
@@ -8,11 +8,13 @@ namespace Expense_Tracker.Models
         [Key]
         public int TransactionId { get; set; }
         public int CategoryId { get; set; }
-        public Category Category { get; set; }
+        public Category? Category { get; set; }
 
         public int Amount { get; set; }
         [Column(TypeName = "nvarchar(75)")]
         public string? Note { get; set; }
         public DateTime Date { get; set; } = DateTime.Now;
+
+
     }
 }
